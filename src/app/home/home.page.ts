b7a155d1f96d796681f456769bc10fe640e61281
Alexander27-GA <alexanderj212703@gmail.com>
2023-01-16 16:42:56 -0500

    nuevo

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,18 +1,12 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
-  slideOpt = {
-    initialSlide: 1, //slide inicial (primero) [0,1,2,3]
-    slidesPerView: 1, //configuramos un slide por vista
-    centerSlides: true, //que las slides enten centradas
-    speed: 400 //velocidad movimiento de los slides
-  }
 
   constructor() {}
 
--- src/app/intro/intro.page.ts
+++ src/app/intro/intro.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-intro',
@@ -6,8 +7,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./intro.page.scss'],
 })
 export class IntroPage implements OnInit {
+  slideOpt = {
+    initialSlide: 1, //slide inicial (primero) [0,1,2,3]
+    slidesPerView: 1, //configuramos un slide por vista
+    centerSlides: true, //que las slides enten centradas
+    speed: 400 //velocidad movimiento de los slides
+  }
+  slides = [
+    {
+      img:"https://cdn-icons-png.flaticon.com/512/2038/2038116.png",
+      title:"Tu Biblioteca virtual",
+      subtitle:"Disfruta de tus libros favoritos"
+    },
+    {
+      img:"https://cdn-icons-png.flaticon.com/512/2888/2888432.png",
+      title:"Busca nuevos Libros",
+      subtitle:"Explora y encuentras nuevos libros para aprender y leer"
+    },
+    {
+      img:"https://cdn-icons-png.flaticon.com/512/3574/3574729.png",
+      title:"Lee donde quieras sin interrucion",
+      subtitle:"Descarga todos tus libros favoritos y lee los donde tu quieras"
+    },
+    {
+      img:"https://cdn-icons-png.flaticon.com/512/5736/5736758.png",
+      title:"Audio Libros",
+      subtitle:"Encuentra los mejores audio libros"
+    }
 
-  constructor() { }
+  ]
+  
+  constructor(private router:Router) { }
+
+  finish(){
+    this.router.navigateByUrl("/home")
+  }
 
   ngOnInit() {
   }
